86bc7c3d34027253722fb62292196442ee01d3ed
Jacob Persson <jacob.persson@wire.com>
2017-05-02 13:12:49 +0200

    Improve property documentation

--- Source/Public/ZMPushChannel.h
+++ Source/Public/ZMPushChannel.h
@@ -20,10 +20,11 @@
 
 @protocol ZMPushChannel <NSObject>
 
-/// When set not to nil an attempt open the push channel will be made
+/// When set not to nil an attempt open the push channel will be made if necessary
 @property (nonatomic, nullable) NSString *clientID;
 
-/// When set to YES the push channel will try to remain open.
+/// When set to YES the push channel will try to remain open and if set to NO it will
+/// close immediately and remain closed.
 @property (nonatomic) BOOL keepOpen;
 
 @end
