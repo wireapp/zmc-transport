7db90b97a14df855e189eaa0e1c273ec742b9f4a
Jacob Persson <jacob.persson@wire.com>
2017-05-09 18:28:52 +0200

    Pin public keys in favor of CA pinning

---
+++ Tests/Source/URLSession/ZMServerTrustTests.m
@@ -0,0 +1,78 @@
+//
+// Wire
+// Copyright (C) 2017 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+#import <XCTest/XCTest.h>
+#import "ZMServerTrust.h"
+
+extern BOOL verifyServerTrust_(SecTrustRef const serverTrust, NSString *host);
+
+@import WireTesting;
+
+@interface ZMServerTrustTests : ZMTBaseTest <NSURLSessionDataDelegate>
+
+@property (nonatomic) NSURLSession *urlSession;
+@property (nonatomic) XCTestExpectation *trustedServer;
+
+@end
+
+@implementation ZMServerTrustTests
+
+- (void)setUp {
+    [super setUp];
+    
+    self.urlSession = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:self delegateQueue:nil];
+}
+
+- (void)tearDown {
+    self.urlSession = nil;
+    
+    [super tearDown];
+}
+
+- (void)testProductionBackendPinning {
+    self.trustedServer = [self expectationWithDescription:@"We are trusting the server"];
+    
+    [[self.urlSession dataTaskWithURL:[NSURL URLWithString:@"https://prod-nginz-https.wire.com/clients/"]] resume];
+    
+    XCTAssertTrue([self waitForCustomExpectationsWithTimeout:5.0]);
+}
+
+- (void)testCDNPinning {
+    self.trustedServer = [self expectationWithDescription:@"We are trusting the server"];
+    
+    [[self.urlSession dataTaskWithURL:[NSURL URLWithString:@"https://d7uri8nf7uskq.cloudfront.net/sample.jpg"]] resume];
+    
+    XCTAssertTrue([self waitForCustomExpectationsWithTimeout:5.0]);
+}
+
+- (void)URLSession:(NSURLSession * __unused)session didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition, NSURLCredential * _Nullable))completionHandler
+{
+    NSURLProtectionSpace *protectionSpace = challenge.protectionSpace;
+    if ([protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+        BOOL const didTrust = verifyServerTrust_(protectionSpace.serverTrust, protectionSpace.host);
+        
+        if (! didTrust) {
+            completionHandler(NSURLSessionAuthChallengeCancelAuthenticationChallenge, nil);
+        } else {
+            [self.trustedServer fulfill];
+        }
+    }
+    completionHandler(NSURLSessionAuthChallengeCancelAuthenticationChallenge, nil);
+}
+
+@end
