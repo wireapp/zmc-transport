4060a83a192a58e133b85041ecda4538c6e3cdbf
Vytis Šibonis <vytis@wire.com>
2018-12-12 15:55:28 +0100

    Updated protocol to add trust function

--- Source/Public/BackendEnvironmentProvider.swift
+++ Source/Public/BackendEnvironmentProvider.swift
@@ -18,8 +18,14 @@
 
 import Foundation
 
+@objc public protocol BackendTrustProvider: NSObjectProtocol {
+    /// Returns true if certificate matches what we expect it to be OR it's a host we don't need to check
+    /// False if certificate doesn't match 
+    @objc func verifyServerTrust(trust: SecTrust, host: String?) -> Bool
+}
+
 // Swift migration notice: this protocol conforms to NSObjectProtocol only to be usable from Obj-C.
-@objc public protocol BackendEnvironmentProvider: NSObjectProtocol {
+@objc public protocol BackendEndpointsProvider: NSObjectProtocol {
     /// Backend base URL.
     var backendURL: URL { get }
     /// URL for SSL WebSocket connection.
@@ -28,7 +34,6 @@ import Foundation
     var blackListURL: URL { get }
     /// Frontent URL, used to open the necessary web resources, like password reset.
     var frontendURL: URL { get }
-    /// Returns true if certificate matches what we expect it to be OR it's a host we don't need to check
-    /// False if certificate doesn't match 
-    func verifyServerTrust(trust: SecTrust, host: String?) -> Bool
 }
+
+@objc public protocol BackendEnvironmentProvider: BackendTrustProvider, BackendEndpointsProvider { }
