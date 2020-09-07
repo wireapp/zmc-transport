ef9baad987d5686823dcdcfdd9eadee3162c27d0
Jacob Persson <jacob.persson@gmail.com>
2020-09-07 08:38:50 +0200

    Clear allTasksEndedHandlers on reset (fixes flaky test)

--- Tests/Source/Background/BackgroundActivityFactoryTests.swift
+++ Tests/Source/Background/BackgroundActivityFactoryTests.swift
@@ -188,6 +188,7 @@ extension BackgroundActivityFactory {
         currentBackgroundTask = nil
         activities.removeAll()
         activityManager = nil
+        allTasksEndedHandlers = []
         mainQueue = .main
     }
 
