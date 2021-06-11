f8771d9bcd42c890de2af661d99a2fe9a47123db
Jacob Persson <jacob.persson@gmail.com>
2021-06-11 15:01:47 +0200

    Increase HTTPMaximumConnectionsPerHost to 4

--- Source/TransportSession/ZMTransportSession.m
+++ Source/TransportSession/ZMTransportSession.m
@@ -111,7 +111,7 @@ static NSInteger const DefaultMaximumRequests = 6;
     // RFC 2616 recommends no more than 2 connections per host when using pipelining.
     // https://tools.ietf.org/html/rfc2616
     configuration.HTTPShouldUsePipelining = NO;
-    configuration.HTTPMaximumConnectionsPerHost = 2;
+    configuration.HTTPMaximumConnectionsPerHost = 4;
     
     configuration.TLSMinimumSupportedProtocol = kTLSProtocol12;
     
